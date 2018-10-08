626e2a5b9cf2f1a3748694ff1cb1f59618a24af0
arju88nair <arju88nair@gmail.com>
2018-10-08 13:14:57 +0530

    Fresh before queue

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use Illuminate\Http\Request;
+use Mail;
+use App\User;
+
+class MailController extends Controller
+{
+
+    public function send()
+    {
+
+
+    }
+}
